77b1c01cd090eb0212fc03dbd86b417caf7ac1ae
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-01-07 13:22:14 +0200

    Updates

---
+++ precache-manifest.71aa5dd6bd087662aa248589bbebdc7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a6e23825a0c14d4ff2f",
+    "url": "/face_recognition_brain/static/css/main.7a9ec4cb.chunk.css"
+  },
+  {
+    "revision": "5a6e23825a0c14d4ff2f",
+    "url": "/face_recognition_brain/static/js/main.5a6e2382.chunk.js"
+  },
+  {
+    "revision": "28fcd41f3ad20dc083d9",
+    "url": "/face_recognition_brain/static/js/runtime~main.28fcd41f.js"
+  },
+  {
+    "revision": "672d23f0dad4053f5c0b",
+    "url": "/face_recognition_brain/static/css/2.b51de973.chunk.css"
+  },
+  {
+    "revision": "672d23f0dad4053f5c0b",
+    "url": "/face_recognition_brain/static/js/2.672d23f0.chunk.js"
+  },
+  {
+    "revision": "de0afa601ab0a053402b6fdc4704304b",
+    "url": "/face_recognition_brain/index.html"
+  }
+];
