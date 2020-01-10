fc4c55c3cfc7cf9c06da8ccfe7aab2fd09ef121c
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-01-10 17:49:44 +0200

    fix NaN bug

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
  const Results = ({ results }) => {
+    if(results.age){
+        var age = results.age-6;
+    }//to prevent NaN appear before fetching results
   return (
     <div className="left ma">
-    <div className='white ml4'><h3>Age:{results.age}</h3>Prob:{results.ageProp}% 
+    <div className='white ml4'><h3>Age:{age}</h3>Prob:{results.ageProp}% 
     <div className='white ml2'><h3>Gender:{results.gender}</h3>Prob:{results.genderProp}% 
     <div className='white ml0 mb3'><h3>Culture:<br/>{results.culture}</h3>Prob:{results.cultureProp}%  
     </div>
